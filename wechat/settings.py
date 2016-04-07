5a66708488fa09265ee04dc4dbf095a0f17475d7
tolerious <tolerious@qq.com>
2016-04-07 14:46:23 +0800

    update...,  我们一起啪啪啪  ;

--- wechat/settings.py
+++ wechat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SITE_SRC_ROOT = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
