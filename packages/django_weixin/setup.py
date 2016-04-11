ddf7d2c6dafe12768e1183724993de2be7ff1ec7
tolerious <tolerious@qq.com>
2016-04-11 15:37:22 +0800

    update...,  我们一起啪啪啪  ;

--- packages/django_weixin/setup.py
+++ packages/django_weixin/setup.py
@@ -18,7 +18,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-weixin',
-    version='0.1.4',
+    version='0.1.5',
     packages=['django_weixin'],
     include_package_data=True,
     license='MIT License',  # example license
