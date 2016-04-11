28d5848e21b86a6db137f051c48c9772b70a18db
tolerious <tolerious@qq.com>
2016-04-11 15:18:51 +0800

    update...,  我们一起啪啪啪  ;

--- packages/django_weixin/setup.py
+++ packages/django_weixin/setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django_weixin',
+    name='django-weixin',
     version='0.1.4',
     packages=['django_weixin'],
     include_package_data=True,
