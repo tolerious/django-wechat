d28daa7e57368fb8dca522f968914fb3ea7b9677
tolerious <tolerious@qq.com>
2016-04-11 15:14:25 +0800

    update...,  我们一起啪啪啪  ;

--- fabfile.py
+++ fabfile.py
@@ -20,6 +20,7 @@ def m(b="master", c=" 我们一起啪啪啪 "):
         local("mkdir -p packages/django_weixin")
         local("cp -R django_weixin packages/django_weixin")
         local("cd packages/django_weixin && python setup.py sdist")
+        local("cd packages/django_weixin/dist/ && rm *")
         local("cd packages/django_weixin && twine upload dist/*")
     with cd("/home/tolerious/wechat_env/django-wechat"):
         run("git stash")
