5ef464e5e4006ac0495cc22b910906c74f4d3488
tolerious <tolerious@qq.com>
2016-04-11 11:37:53 +0800

    update...,  我们一起啪啪啪  ;

--- django-weixin/setup.py
+++ django-weixin/setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-wechat',
+    name='django-weixin',
     version='0.1',
     packages=['app'],
     include_package_data=True,
