3d27342916ff06b37dd4269b44a7861b60efd633
tolerious <tolerious@qq.com>
2016-04-12 16:14:07 +0800

    我们都是好孩子.  ;

--- wechat/urls.py
+++ wechat/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wechat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'django-weixin/',include('django_weixin.urls')),
-    # url(r'^validate/', include('django_weixin.urls', namespace='djangoweixin')),
-    url(r'^access/', include('django_weixin.urls')),
-    # url(r'api/', include('apis.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-
-)
+                       # Examples:
+                       # url(r'^$', 'wechat.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'django-weixin/', include('django_weixin.urls')),
+                       # url(r'^validate/', include('django_weixin.urls', namespace='djangoweixin')),
+                       url(r'^access/', include('django_weixin.urls')),
+                       # url(r'api/', include('apis.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
