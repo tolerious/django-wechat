a0fc57db7ef3457847fe1fa7860e18f66c7340bc
tolerious <tolerious@qq.com>
2016-04-12 17:22:04 +0800

    我们都是好孩子.  ;

--- django_weixin/static/js/menu_config.js
+++ django_weixin/static/js/menu_config.js
@@ -2,10 +2,9 @@
  * Created by fengxiaolong on 16/4/12.
  */
 $(function () {
-    generate_create_menu_json();
 });
 
-function generate_create_menu_json() {
+function confirm_button_click() {
     var final_string = "";
     for (i = 1; i < 16; i++) {
         final_string  =  $("#sub_" + i).val() + ",";
