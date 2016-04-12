8673bd19ecbc63a189b11a953c03cf0c67fbac27
tolerious <tolerious@qq.com>
2016-04-12 17:25:48 +0800

    我们都是好孩子.  ;

--- django_weixin/static/js/menu_config.js
+++ django_weixin/static/js/menu_config.js
@@ -6,9 +6,9 @@ $(function () {
 
 function confirm_button_click() {
     var final_string = "";
+    var final_array = new Array();
     for (i = 1; i < 16; i++) {
-        final_string  =  $("#sub_" + i).val() + ",";
-        console.log($("#sub_"+i).val());
+        final_array.append($("#sub_" + i).val());
     }
-    console.log(final_string);
+    console.log(final_array);
 }
