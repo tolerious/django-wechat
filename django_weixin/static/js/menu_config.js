155c6c353c2a357bb7bac43c02cf50ead90b9c87
tolerious <tolerious@qq.com>
2016-04-13 15:00:26 +0800

    我们都是好孩子.  ;

--- django_weixin/static/js/menu_config.js
+++ django_weixin/static/js/menu_config.js
@@ -14,15 +14,15 @@ function confirm_button_click() {
     var name_3_array = new Array();
     for (i = 1; i < 16; i++) {
         if (i < 6) {
-            name_1_array.push($("name_" + i).val());
+            name_1_array.push($("#name_" + i).val());
             menu_1_array.push($("#sub_" + i).val());
         }
         if (i > 5 && i < 11) {
-            name_2_array.push($("name_" + i).val());
+            name_2_array.push($("#name_" + i).val());
             menu_2_array.push($("#sub_" + i).val());
         }
         if (i > 10) {
-            name_3_array.push($("name_" + i).val());
+            name_3_array.push($("#name_" + i).val());
             menu_3_array.push($("#sub_" + i).val());
         }
     }
