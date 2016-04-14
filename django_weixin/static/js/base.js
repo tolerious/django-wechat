6336b1845efb0f674aa7b008f7dc97d525b3e615
tolerious <tolerious@qq.com>
2016-04-14 15:26:38 +0800

    我们都是好孩子.  ;

---
+++ django_weixin/static/js/base.js
@@ -0,0 +1,31 @@
+/**
+ * Created by tolerious on 15/9/25.
+ */
+
+function getCookieValue(cookieName)  // get cookie value
+{
+    var cookieValue = document.cookie;
+    var cookieStartAt = cookieValue.indexOf("" + cookieName + "=");
+    if (cookieStartAt == -1) {
+        cookieStartAt = cookieValue.indexOf(cookieName + "=");
+    }
+    if (cookieStartAt == -1) {
+        cookieValue = null;
+    }
+    else {
+        cookieStartAt = cookieValue.indexOf("=", cookieStartAt) + 1;
+        cookieEndAt = cookieValue.indexOf(";", cookieStartAt);
+        if (cookieEndAt == -1) {
+            cookieEndAt = cookieValue.length;
+        }
+        cookieValue = unescape(cookieValue.substring(cookieStartAt, cookieEndAt));//解码latin-1
+    }
+    return cookieValue;
+}
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
