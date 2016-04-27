be7a23ce05374e544571184f2bdef74f6f861805
tolerious <tolerious@qq.com>
2016-04-27 11:10:38 +0800

    我们都是好孩子.  ;

--- django_weixin/utils/utils.py
+++ django_weixin/utils/utils.py
@@ -5,9 +5,10 @@ Created on 04 08, 2016
 @author: tolerious
 
 '''
-import datetime,requests,json
+import datetime, requests, json
 import json, xml.etree.ElementTree as et
 from django_weixin.WXBizMsgCrypt import *
+
 
 def python_time_to_javascript(python_datetime):
     if python_datetime is not None:
@@ -36,11 +37,13 @@ def judge_message_is_valid(access_token, aeskey, corpid, signature, timestamp, n
     ret, return_token = wx.VerifyURL(signature, timestamp, nonce, echostr)
     return ret, return_token  # if ret == 0, valid
 
-def generate_create_base_menu_button_json():  #生成基础功能的button,click,view类型的button
+
+def generate_create_base_menu_button_json():  # 生成基础功能的button,click,view类型的button
     base_json = {
-        "button":[]
+        "button": []
     }
     return base_json
+
 
 def get_follower_info():
     from django_weixin.models.basic import *
@@ -48,5 +51,6 @@ def get_follower_info():
     a = AccessToken.objects.get(pk=1)
     access_token = a.get_access_token()
     new_url = url.format(access_token=access_token)
+    print new_url
     r = requests.get(new_url)
     print r.json()
